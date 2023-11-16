4ad39dd3d29cfd3daba838804bf76004c5feb2fa
Hüseyin Yener <huseyin.yener@bestcloudfor.me>
2023-11-16 14:11:29 +0300

    SonarQube

--- app/src/main/java/com/example/androidjenkinsapp/MainActivity.java
+++ app/src/main/java/com/example/androidjenkinsapp/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.androidjenkinsapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private WebView webView;
 
     @Override
@@ -13,10 +15,23 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // WebView'i bul
         webView = findViewById(R.id.webView);
 
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.bestcloudfor.me");
+        // WebView için ayarları yapılandır
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // JavaScript'i etkinleştir
+
+        // WebViewClient kullanarak WebView'i ayarla
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                Log.e("WebView Error", "Error Code: " + errorCode + ", Description: " + description);
+            }
+        });
+
+        // WebView'e bir web sayfasını yükle
+        webView.loadUrl("https://bestcloudfor.me");
     }
 }
